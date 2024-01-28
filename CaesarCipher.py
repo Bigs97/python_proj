d0d624fd4a045951a0500b8bd425356d06b65a07
Bigyat <bigyatbtr@gmail.com>
2024-01-28 15:33:35 +0545

    just update

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,24 +1,25 @@
 import string
 
 def main():
-  sentence = input('Please enter the sentence to be ciphered: ').strip().lower().split(sep = ' ')
+  sentence = input('Please enter the sentence to be ciphered: ').strip().lower()
   print(sentence)
   plainToCipherText(sentence)
   cipherToPlainText()
 
 def plainToCipherText(sentence):
   word_to_num = {word:num for num,word in enumerate(string.ascii_lowercase)}
+  word_to_num[' '] = 26
+  print(word_to_num)
   lst_of_num = []
   new_lst_of_num = []
   brand_new_lst_of_num = []
   for i in sentence:
-    for j in i:
-      if j in word_to_num:
-        lst_of_num.append(word_to_num[j])
+    if i in word_to_num:
+      lst_of_num.append(word_to_num[i])
   print(lst_of_num)
   
   for i in lst_of_num:
-    new_lst_of_num.append((i+3)%26)
+    new_lst_of_num.append((i+3)%27)
   print(new_lst_of_num)
 
   for i in new_lst_of_num:
