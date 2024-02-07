e966a885bc669d580a60e9d570725bbb130408ad
Bigyat <bigyatbtr@gmail.com>
2024-02-07 15:51:59 +0545

    Some Changes.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,31 +1,22 @@
 import string
 
 word_to_num = {word:num for num,word in enumerate(string.ascii_lowercase)}
-print(word_to_num)
-# word_to_num[' '] = 26
 
 def main():
   sentence = input('Please enter the sentence to be ciphered: ').strip().lower()
-  print(sentence)
   plainToCipherText(sentence)
   cipherToPlainText()
 
 def plainToCipherText(sentence):
-  lst_of_num = []
-  new_lst_of_num = []
   brand_new_lst_of_num = []
-  for i in sentence:
-    if i in word_to_num:
-      lst_of_num.append(word_to_num[i])
-  print(lst_of_num)
-  
-  for i in lst_of_num:
-    new_lst_of_num.append((i+3)%26)
-  print(new_lst_of_num)
-
-  for i in new_lst_of_num:
-    if i in word_to_num.values():
-      brand_new_lst_of_num.append(word_to_num)
+  lst_of_num = [word_to_num[i] for i in sentence if i in word_to_num]
+  lst_of_num = [(i+3)%26 for i in lst_of_num]
+  # for alpha, nums in word_to_num.items():
+  #   if nums in word_to_num.values():
+  #     brand_new_lst_of_num.append(alpha)
+    
+  # print(brand_new_lst_of_num)
+  # print(brand_new_lst_of_num)
   # return new_lst_of_num
 
 def cipherToPlainText():
